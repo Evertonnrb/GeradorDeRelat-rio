113c2bf04665d8885921e2c2bf4af029f9db2bd9
Evertonnrb <everton.nrb@hotmail.com>
2016-07-12 23:07:02 -0400

    Gerador de relatório em c#

---
+++ RelatorioReport/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace RelatorioReport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Enviar = new System.Windows.Forms.Button();
+            this.Nome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Enviar
+            // 
+            this.Enviar.Location = new System.Drawing.Point(429, 51);
+            this.Enviar.Name = "Enviar";
+            this.Enviar.Size = new System.Drawing.Size(75, 23);
+            this.Enviar.TabIndex = 0;
+            this.Enviar.Text = "Enviar";
+            this.Enviar.UseVisualStyleBackColor = true;
+            this.Enviar.Click += new System.EventHandler(this.t_Click);
+            // 
+            // Nome
+            // 
+            this.Nome.Location = new System.Drawing.Point(59, 53);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(346, 20);
+            this.Nome.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 261);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.Enviar);
+            this.Name = "Form1";
+            this.Text = "Formulario ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Enviar;
+        private System.Windows.Forms.TextBox Nome;
+    }
+}
+
---
+++ RelatorioReport/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// adicionando as dll's para trabalhar com o report
+using Microsoft.Reporting.WebForms;
+using System.IO;//pegar as pastas temp
+using System.Diagnostics;//abrir pdf
+
+namespace RelatorioReport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void t_Click(object sender, EventArgs e)
+        {
+            //Implementando o botão gerar utilizando a classe para visualizar relatórios
+            ReportViewer reportViewer = new ReportViewer();
+            //processando com enum ProcessingMode
+            reportViewer.ProcessingMode = ProcessingMode.Local;
+            //especificando caminho para encontrar o relatório 
+            reportViewer.LocalReport.ReportEmbeddedResource = "RelatorioReport.Relatorio.rdlc";
+            //Parametros do relatorio
+            List<ReportParameter> listReportParameter = new List<ReportParameter>();
+            //Criar novos parametros para enviar !!!
+            listReportParameter.Add(new ReportParameter("Nome", Nome.Text));
+            reportViewer.LocalReport.SetParameters(listReportParameter);
+            // Renderizando o relatório
+            //output método render
+            Warning[] warnings;
+            string[] streamids;
+            string mineType;
+            string encoding;
+            string extension;
+
+            //add combo para esclolha de como deseja renderizar
+
+            //pdf
+            byte[] bytePDF = reportViewer.LocalReport.Render(
+                "Pdf",null, out mineType, out encoding,out extension, out streamids, out warnings
+                );
+            FileStream fileStreamPDF = null;
+            string nomeArquivoPdf = Path.GetTempPath() + "Relatório" + DateTime.Now.ToString("dd_MM_yyyy-HH_mm_ss") + ".pdf";
+            fileStreamPDF = new FileStream(nomeArquivoPdf, FileMode.Create);
+            fileStreamPDF.Write(bytePDF, 0, bytePDF.Length);
+            fileStreamPDF.Close();
+            Process.Start(nomeArquivoPdf);
+
+            //Exel
+            byte[] byteExcel = reportViewer.LocalReport.Render(
+                "Excel", null, out mineType, out encoding, out extension, out streamids, out warnings
+                );
+
+            FileStream fileStreamExcel = null;
+
+            string nomeArquivoExcel = Path.GetTempPath() + "Relatório" + DateTime.Now.ToString("dd_MM_yyyy-HH_mm_ss") + ".xls";
+            fileStreamExcel = new FileStream(nomeArquivoExcel, FileMode.Create);
+            fileStreamExcel.Write(byteExcel, 0, byteExcel.Length);
+            fileStreamExcel.Close();
+            Process.Start(nomeArquivoExcel);
+
+        }
+    }
+}
